a6a0f39cdd5558b9c526ccebfe347e1401284073
Dhayanithi304 <145765184+Dhayanithi304@users.noreply.github.com>
2024-09-08 00:29:28 +0530

    modified

--- Day_2/server.js
+++ Day_2/server.js
@@ -2,45 +2,48 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-const app = express();
+const app = express()
 
 const outputFolder = "./output";
 
-if (!fs.existsSync(outputFolder)) {
+if(!fs.existsSync(outputFolder)){
   fs.mkdirSync(outputFolder);
 }
 
+app.get("/create_file", (req, res)=>{
+  const currentDate = new Date();
+  const year = currentDate.getFullYear().toString()
+  const month = (currentDate.getMonth()+1).toString()
+  const date = currentDate.getDate().toString()
+  const hrs = currentDate.getHours().toString()
+  const mins = currentDate.getMinutes().toString()
+  const secs = currentDate.getSeconds().toString()
 
+  const datetimeFilename = `${year}-${month}-${date}-${hrs}-${mins}-${secs}.txt`;
 
-app.get("/create_file", (req, res) => {
-    const currentTime = new Date()
-    const year = currentTime.getFullYear().toString();
-    const month = (currentTime.getMonth() + 1).toString();
-    const date = currentTime.getDate().toString();
-    const hrs = currentTime.getHours().toString();
-    const mins = currentTime.getMinutes().toString();
-    const secs = currentTime.getSeconds().toString();
+  const filePath = path.join(outputFolder, datetimeFilename);
 
-    const dateTimeFileName = `${year}-${month}-${date}-${hrs}-${mins}-${secs}.txt`;
-    
-    const filePath = path.join(outputFolder, dateTimeFileName)
+  fs.writeFile(filePath, currentDate.toISOString(), (err)=>{
+    if(err){
+      res.status(500).send(`error - `, err);
+      return;
+    }
+    res.send(`File create succesfully ${filePath}`);
+  })
+});
 
-    fs.writeFile(filePath, currentTime.toISOString(), (err) =>{
-        if(err){
-            res.status(500).send(`File creating error - ${err}`);
-            return;
-        }
-        res.send(`File Created Successfully - ${dateTimeFileName}`)
-    })
-})
-
-app.get("/get_files", (req, res)=>{
-    
-    // const textFile = 
+app.get("/get_files", (req, res) =>{
+  fs.readdir(outputFolder, (err, files)=>{
+    if(err){
+      res.status(500).send("error - ", err);
+    } 
+    const textFile = files.filter(file=>path.extname(file) === ".txt");
+    res.send(textFile);
+  })
 })
 
 const PORT = 3000;
 
-app.listen(PORT, () => {    
-  console.log("Server is running on PORT,", PORT);
-});
+app.listen(PORT, ()=>{
+  console.log(`Server runing on PORT-`+PORT);
+})
